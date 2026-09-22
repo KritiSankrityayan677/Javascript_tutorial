131c561bd239d3d93c0d2390fca65b02aa63280d
Kriti Pandey <kritipandey689@gmail.com>
2026-09-22 15:10:22 +0530

    Basics of datatypes

---
+++ Basics/02_datatypes.js
@@ -0,0 +1,14 @@
+"use scrict"; // treat all js code as newer version of js
+
+// alert(3+3) // we are using node.js here not browser so alert will not work here
+
+let name = "kriti" // it is a string data type
+let age = 18 // it is a number data type
+let isLoggedIn = true // it is a boolean data type
+
+// null and undefined are also data types in js but null is a standalone value in itself that tells this variable isn't undefined it is empty.
+// symbol -> For uniqueness
+// object
+
+console.log(typeof null)
+// Type of null is "object" but type of undefined is "undefined" 
